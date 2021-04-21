40ff7d212b21420d39c5b2b5d024735136f5dd13
Ashish Yadav <31069071+ashishyadav0019@users.noreply.github.com>
2021-04-21 16:47:46 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,13 @@
+var str = "we should follow the programming expert "
+var strObj = {};
+var outStr = "";
+for(var i =0; i<str.length; i++){
+strObj[i] = str[i]
+}
+strObj[0] = strObj[0].toUpperCase()
+for(var j=0; j<str.length; j++){
+    if(strObj[j].charCodeAt()===32){
+         if(strObj[j+1]) strObj[j+1] = strObj[j+1].toUpperCase()
+    }
+outStr += strObj[j]
+}
