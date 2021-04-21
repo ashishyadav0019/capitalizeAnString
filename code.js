9776f50c7d96eb14d3f45db6796625caeb77835f
Ashish Yadav <31069071+ashishyadav0019@users.noreply.github.com>
2021-04-21 16:49:09 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,15 @@
-var str = "we should follow the programming expert "
-var strObj = {};
-var outStr = "";
-for(var i =0; i<str.length; i++){
-strObj[i] = str[i]
-}
-strObj[0] = strObj[0].toUpperCase()
-for(var j=0; j<str.length; j++){
-    if(strObj[j].charCodeAt()===32){
-         if(strObj[j+1]) strObj[j+1] = strObj[j+1].toUpperCase()
+function capitalize(str){
+    var strObj = {};
+    var outStr = "";
+    for(var i =0; i<str.length; i++){
+        strObj[i] = str[i]
     }
-outStr += strObj[j]
+    strObj[0] = strObj[0].toUpperCase()
+    for(var j=0; j<str.length; j++){
+        if(strObj[j].charCodeAt()===32){
+             if(strObj[j+1]) strObj[j+1] = strObj[j+1].toUpperCase()
+        }
+    outStr += strObj[j]
+    }
 }
+capitalize("we should follow the programming expert")
